259b775dd56eae5adfb3659ebfbace2229633288
richard579 <richardbeng358@outlook.com>
2023-06-27 03:52:42 -0700

    3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -11,12 +11,12 @@ void quick_sort(int *array, size_t size);
  * @b: The second integer to swap.
  */
 void swap_ints(int *a, int *b)
-{       
-        int tmp;
-        
-        tmp = *a;
-        *a = *b;
-        *b = tmp;
+
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 
 /**
